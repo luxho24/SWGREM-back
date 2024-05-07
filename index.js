2dccc6b32fb658064b58c085b91951791f2cd4c2
luxho31 <202111482@urp.edu.pe>
2024-05-07 00:21:06 -0500

    feat: Correccion de error entre (Ramas: Jesus y Luis)

--- index.js
+++ index.js
@@ -29,5 +29,5 @@ app.use("/api/usuarios", usuarioRoutes);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en: http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en: http://127.0.0.1:${PORT}`);
 });
